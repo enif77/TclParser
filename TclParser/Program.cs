235983e410f4d76b60d3f1a09265889366d86c86
enif77 <premysl.fara@gmail.com>
2022-08-11 21:31:28 +0200

    Bracketed word basic.

--- TclParser/Program.cs
+++ TclParser/Program.cs
@@ -96,6 +96,15 @@ public class Tokenizer : ITokenizer
                 );
             }
             
+            if (c == '{')
+            {
+                // Not in the middle of a word?
+                if (buffer.Length == 0)
+                {
+                    return CollectBracketedWord();
+                }
+            }
+            
             buffer.Append((char)c);
 
             _ = _reader.NextChar();
@@ -108,10 +117,9 @@ public class Tokenizer : ITokenizer
 
     /// <summary>
     /// Generates a token from the collected chars buffer.
-    /// Note: If the collected chars buffer is empty, the current char in the reader is consumed!
     /// </summary>
     /// <param name="buffer">A chars buffer, that contains so far collected chars, that will be returned as the Text token.</param>
-    /// <param name="whenEmpty">An action, that will be executed, when nothing was executed so far.</param>
+    /// <param name="whenEmpty">An action, that will be executed, when nothing was collected so far.</param>
     /// <returns>A token.</returns>
     private IToken GenerateToken(StringBuilder buffer, Func<IToken> whenEmpty)
     {
@@ -171,6 +179,41 @@ public class Tokenizer : ITokenizer
 
             c = _reader.NextChar();
         }
+    }
+    
+    
+    private IResult<IToken>CollectBracketedWord()
+    {
+        // Consume the bracketed word start char...
+        var c = _reader.NextChar();
+
+        // TODO: Add support for "\{" and multiple levels of {aa {ee} bb}.
+
+        // and consume all chars till the nearest '}' or EoF.
+        var buffer = new StringBuilder();
+        while (true)
+        {
+            if (c == '}')
+            {
+                // Eat the bracketed word end.
+                _ = _reader.NextChar();
+
+                break;
+            }
+            
+            if (IsEoF(c))
+            {
+                return Result<IToken>.Error("The '}' bracketed word end is missing.");
+            }
+
+            buffer.Append((char)c);
+
+            c = _reader.NextChar();
+        }
+
+        return Result<IToken>.Ok(
+            CurrentToken = Token.WordToken(buffer.ToString())
+        );
     }
 }
 
